4c86aa4c2abc250f250a151b237a07e6aebab05e
Thanh Pham <phamqthanh2018@gmail.com>
2022-07-09 14:59:13 +0700

    fix: update build

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as AutoSticky } from "./sticky/AutoSticky";
-export * from "./sticky/AutoSticky";
--- dist/sticky/AutoSticky.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export declare enum ScrollType {
-    UP = 0,
-    DOWN = 1
-}
-declare const AutoSticky: () => {
-    ref: React.RefObject<HTMLDivElement>;
-};
-export default AutoSticky;
